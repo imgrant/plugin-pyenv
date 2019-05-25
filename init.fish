4901118026497bcf30cc51eecd26cef8cc4a01e5
Ian G <imgrant@users.noreply.github.com>
2019-05-25 20:49:22 +0100

    Update init.fish
    Modify init.fish as https://github.com/rominf/omf-plugin-pyenv (originally a Fisher pyenv plugin)

--- init.fish
+++ init.fish
@@ -1,4 +1,21 @@
-set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
+set -l pyenv_root ""
 
-set PATH $PYENV_ROOT/libexec/pyenv $PYENV_ROOT/libexec $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH 2> /dev/null
-setenv PYENV_SHELL fish
+if test -z "$PYENV_ROOT"
+    set pyenv_root $HOME/.pyenv
+    set -xg PYENV_ROOT "$pyenv_root"
+else
+    set pyenv_root "$PYENV_ROOT"
+end
+
+set -xg PATH "$pyenv_root/bin" $PATH
+
+if not command -s pyenv > /dev/null
+    echo "Install <https://github.com/yyuu/pyenv> to use 'pyenv'."
+    exit 1
+end
+
+if status --is-interactive
+    set -xg PATH "$pyenv_root/shims" $PATH
+    set -xg PYENV_SHELL fish
+end
+command mkdir -p "$pyenv_root/"{shims,versions}
